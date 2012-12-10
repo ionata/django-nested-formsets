59092b75cb5ded6020e4c68836182022336f0898
Tim Heap <tim@ionata.com.au>
2012-12-10 14:05:40 +1100

    Version bump to v0.1.1

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ except ImportError:
 
 setup(
     name='django-nested-formsets',
-    version="0.1.0",
+    version="0.1.1",
     description='Simple nested formsets in a ModelForm',
     author='Ionata Web Solutions',
     author_email='webmaster@ionata.com.au',
